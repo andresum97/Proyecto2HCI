ac127c1550f328072b9c433e638935a539795fe9
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2019-05-20 20:27:39 -0600

    get data actualizado

--- Hardware/get_sensor_data.ino
+++ Hardware/get_sensor_data.ino
@@ -1,36 +1,28 @@
-#include <Servo.h>
-#include <Stepper.h>
-
-#define PLC_IN A0
-#define PLC_UP A1
-#define PLC_DOWN A2
-#define PLC_STOP A3
-
-Servo get_data;
-Stepper GetData(256, 13, 12, 11, 10);
-unsigned int new_data, angulo,step_up,step_down;
+#include <stdint.h>
+#define PIN A0
+bool gate_control;
+uint16_t people_in;
+String message, quantity;
 
 void setup() {
-
-  pinMode(PLC_UP, INPUT);
-  pinMode(PLC_DOWN, INPUT);
-  pinMode(PLC_STOP, INPUT);
-  pinMode(2,OUTPUT);
-  get_data.attach(8);
   Serial.begin(9600);
-  digitalWrite(2,HIGH);
-  GetData.setSpeed(20);
+  pinMode(PIN, INPUT);
+  people_in = 0;
+  gate_control = false;
+  message = "Initialized";
+  Serial.println(message);
 }
 
 void loop() {
-  new_data = analogRead(PLC_IN);
-  angulo = map(new_data, 200, 1023, 0, 30);
-  angulo = map(angulo, 0, 30, 0, 180);
-  get_data.write(angulo);
-  Serial.print("Abierto: ");
-  Serial.print(map(angulo,0,180,0,100));
-  Serial.println("%");
-  delay(25);
-
-    
+  if (digitalRead(PIN) == LOW)
+  {
+    people_in ++;
+    while (digitalRead(PIN) == LOW) {
+      Serial.println("Entrando");
+    }
+  }
+  quantity = String(people_in);
+  message = "Personas adentro: " + quantity;
+  Serial.println(message);
+  delay(250);
 }
