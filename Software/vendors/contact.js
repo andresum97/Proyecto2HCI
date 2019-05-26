ebc02286fddb8623f29d7d4ccc55b995c1b85b0c
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2019-05-25 23:57:17 -0600

    validaciones en contacto

---
+++ Software/vendors/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "Envía una respuesta correcta");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 8
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Nombre es un campo requerido",
+                    minlength: "Tu nombre debe ser consistente o al menos 2 caracteres"
+                },
+                subject: {
+                    required: "Asunto es un campo requerido",
+                    minlength: "Tu Asunto debe ser consistente o al menos 4 caracteres."
+                },
+                number: {
+                    required: "Número es un campo requerido",
+                    minlength: "Su número debe ser consistente o al menos 8 caracteres"
+                },
+                email: {
+                    required: "Correo electrónico es un campo requerido"
+                },
+                message: {
+                    required: "Mensaje es un campo requerido",
+                    minlength: "Debes de enviar más información un mínimo de 20 palabras"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
