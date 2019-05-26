7df39bc280bedf8a7f259a03617da7e2b3e3d225
Andres Urizar <uri17632@uvg.edu.gt>
2019-05-25 22:30:32 -0600

    leer datos e inicio de graficar datos
    inicio de ingreso de datos

--- Software/src/firebase.js
+++ Software/src/firebase.js
@@ -1,4 +1,7 @@
 var firebase = require("firebase");
+var Chart = require('chart.js');
+
+src="https://www.gstatic.com/firebasejs/6.0.4/firebase-app.js"
 
 const firebaseConfig = {
     apiKey: "AIzaSyB7Hjdc5RX9gQ_HJYRDS2stQhNs0NzGvxc",
@@ -9,3 +12,59 @@ const firebaseConfig = {
     messagingSenderId: "1003979180833",
     appId: "1:1003979180833:web:83a44405a22bb4dd"
   };
+
+  firebase.initializeApp(firebaseConfig);
+
+  var db= firebase.database();
+  var fecharef = firebase.database().ref().child("fecha");
+ 
+  // console.log(firebase.auth) // Undefined
+  // console.log(firebase.default.auth) // Function
+  function leerDatostotales(){
+    fecharef.on("value",function(snapshot){
+      var prueba = snapshot.val();
+      totaldatos = 0;
+      var key = Object.keys(snapshot.val());
+      console.log(snapshot.val());
+      console.log(key);
+      for(i=0;i<key.length;i++){
+        console.log(key[i]);
+        totaldatos = i+1;
+      }
+      console.log('Cantidad de datos totales: '+totaldatos);
+    },function(errorObject){
+      console.log("The read failed: "+errorObject.code);
+    });
+    // var fecharef = firebase.database().ref().child("fecha");
+    // console.log('primero');
+    // fecharef.on('value',function(snapshot){
+    //   key = Object.keys(snapshot.val());
+    //   console.log(key);
+    //   for(i=0;i<key.length;i++){
+    //     id = key[i];
+    //     prueba += ',',id;
+      
+    //   }
+    // });
+  }
+  function datosFecha(fecha){
+    fecharef.on("value",function(snapshot){
+      var key = Object.keys(snapshot.val());
+      for(i=0;i<key.length;i++){
+        // console.log('funciona');
+        var id = key[i];
+        var conn2 = firebase.database().ref('fecha/'+id);
+        conn2.on('value',function(snapshot){
+          connfecha = snapshot.val().fecha;
+          if(connfecha == fecha){
+           console.log(connfecha+","+snapshot.val().hora);
+          }
+        });
+      }
+    },function(errorObject){
+      console.log("The read failed "+errorObject.code);
+    });
+  }
+  // leerDatostotales();
+  datosFecha('26/05/19');
+  //console.log(leerDatostotales());
